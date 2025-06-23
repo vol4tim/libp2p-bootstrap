50ba670880a6fbd8bc6020db6d5227e40d50045b
vol4tim <vol4tim@gmail.com>
2025-06-23 13:57:24 +0400

    Initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,53 @@
+import { noise } from "@chainsafe/libp2p-noise";
+import { yamux } from "@chainsafe/libp2p-yamux";
+import { privateKeyFromRaw } from "@libp2p/crypto/keys";
+import { identify } from "@libp2p/identify";
+import { kadDHT, removePublicAddressesMapper } from "@libp2p/kad-dht";
+import { ping } from "@libp2p/ping";
+import { tcp } from "@libp2p/tcp";
+import { webSockets } from "@libp2p/websockets";
+import { createLibp2p } from "libp2p";
+import { fromString as uint8ArrayFromString } from "uint8arrays/from-string";
+
+const privateKey =
+  "1uIJIFyOztdjxTBQauQVYRmf6P89xqSnWNWd+FLfaNfrDeyu4DwfhsgYrLyqj1fHnsPVsrjeuQ7OW+G2bRpu9g";
+
+async function createNode(options) {
+  return await createLibp2p({
+    ...options,
+    streamMuxers: [yamux()],
+    connectionEncrypters: [noise()],
+    services: {
+      identify: identify(),
+      ping: ping(),
+      dht: kadDHT({
+        clientMode: false,
+        protocol: "/ipfs/lan/kad/1.0.0",
+        peerInfoMapper: removePublicAddressesMapper
+      })
+    }
+  });
+}
+
+async function startServer() {
+  const node1 = await createNode({
+    privateKey: privateKeyFromRaw(uint8ArrayFromString(privateKey, "base64")),
+    addresses: {
+      listen: ["/ip4/0.0.0.0/tcp/4111", "/ip4/0.0.0.0/tcp/2111/ws"]
+    },
+    transports: [tcp(), webSockets()]
+  });
+  await node1.start();
+  console.log("Node 1 started with ID:", node1.peerId.toString());
+  console.log(
+    "Node 1 listening on:",
+    node1.getMultiaddrs().map(addr => addr.toString())
+  );
+  node1.addEventListener("peer:connect", () => {
+    setTimeout(() => {
+      console.log("PEERS ON NODE 1:", node1.getPeers());
+    }, 3000);
+  });
+}
+
+startServer().catch(console.error);
